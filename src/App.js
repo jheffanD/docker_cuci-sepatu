7fe569e12293bd1c31132f6f9ba50cc6a47228a5
jheffanD <jeffandasa86@gmail.com>
2024-05-24 01:00:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-4-xl text-indigo-500">
+        react mas
+      </h1>
+    </main>
+  );
+}
+
+export default App;
