f9315c5aa81904e14eb7cdf49e40beec761a87d6
jheffanD <jeffandasa86@gmail.com>
2024-06-01 23:59:37 +0700

    modified:   src/App.js 	modified:   src/componen/navbar.jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./componen/navbar";
 
 function App() {
   return (
-    <div className="bg-slate-700 bg-center">
+    <div className="bg-slate-700 bg-center w-auto h-20">
       <Navbar/>
     </div>
   );
--- src/componen/navbar.jsx
+++ src/componen/navbar.jsx
@@ -1,41 +1,7 @@
-
-function Navbar(){
+"use client"
+export default function Navbar(){
 
     <main>
-         {/* <!-- Start header section --> */}
-    <header class="header-area" id="header-area">
-        <div class="dope-nav-container breakpoint-off">
-            <div class="container">
-                <div class="row">
-                    {/* <!-- dope Menu --> */}
-                    <nav class="dope-navbar justify-content-between" id="dopeNav">
-
-                        {/* <!-- Logo --> */}
-                        <a class="nav-brand" href="https://shoesandcare.com">
-                            <img width="50px" src="https://shoesandcare.com/assets/front/img/logo/sac-black.webp" alt="logo shoes and care"  height="100%" />
-                        </a>
-
-                        {/* <!-- Navbar Toggler --> */}
-                        <div class="dope-navbar-toggler">
-                            <span class="navbarToggler">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
-                        </div>
-
-                        {/* <!-- Menu --> */}
-                        <div class="dope-menu">
-
-                            {/* <!-- close btn --> */}
-                            <div class="dopecloseIcon">
-                                <div class="cross-wrap">
-                                    <span class="top"></span>
-                                    <span class="bottom"></span>
-                                </div>
-                            </div>
-
-                            {/* <!-- Nav Start --> */}
                             <div class="dopenav">
                                 <ul id="nav">
                                     <li>
@@ -61,17 +27,8 @@ function Navbar(){
                                     </li>
                                 </ul>
                             </div>
-                            {/* <!-- Nav End --> */}
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-
     </main>
 
 
 }
 
-export default Navbar;
