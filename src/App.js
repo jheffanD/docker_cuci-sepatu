6e969ac21e9f2037bd9f8d55ea17855be97ed52f
jepan <jheffanD.com>
2024-06-04 14:45:48 +0700

    modified:   src/App.js 	modified:   src/componen/navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Navbar from "./componen/navbar";
+// import Navbar from "./componen/navbar";
 
 export default function App() {
   return (
-    <>
-    <Navbar/>
-    </>
+    <div className="flex bg-slate-300 h-screen">
+      <div className="bg-slate-600 h-14  w-[80rem]">
+        <div>
+        </div>
+      </div>
+   </div>
   );
 }
 
--- src/componen/navbar.jsx
+++ src/componen/navbar.jsx
@@ -1,32 +1,9 @@
-"use client"
+import React from "react"
+
 export default function Navbar(){
 
     <main>
-                            <div class="dopenav">
-                                <ul id="nav">
-                                    <li>
-                                        <a href="/">Beranda</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://shoesandcare.com/about-shoes-and-care">Tentang</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://shoesandcare.com/blog">Blog</a>
-                                    </li>
-									<li>
-                                        <a href="https://shoesandcare.com/lokasi-workshop">Lokasi</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://shoesandcare.com/produk">Produk</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://shoesandcare.com/hasil-workshop">Galeri Kami</a>
-                                    </li>
-                                    <li>
-                                        <a class="pricelist" href="https://shoesandcare.com/pricelist-shoes-and-care">Harga Menu</a>
-                                    </li>
-                                </ul>
-                            </div>
+        <h1>hai ganteng</h1>
     </main>
 
 
