217bfa0704d7587836e8630c6d49a8d22f38c5ae
jepan <jheffanD.com>
2024-06-07 00:29:47 +0700

    modified:   src/pages/Dashboard/input.jsx 	modified:   src/pages/Dashboard/page.jsx

--- src/pages/Dashboard/page.jsx
+++ src/pages/Dashboard/page.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../../componen/navbar"
+import Input from "./input"
+
 
 export default function Dashboard(){
 
     return(
         <div>
             <Navbar/>
+            <Input/>
         </div>
     );
         
