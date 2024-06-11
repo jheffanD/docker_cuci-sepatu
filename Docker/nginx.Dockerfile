1ab6bb1dd9536ce02b59f48d72525f454273e10d
jheffanD <jeffandasa86@gmail.com>
2024-06-11 21:59:18 +0700

    modified:   Docker/Dockerfile.mysql 	modified:   Docker/Dockerfile.node 	modified:   Docker/nginx.Dockerfile 	modified:   docker-compose.yaml 	modified:   env/mysql.env 	modified:   nginx.conf 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- Docker/nginx.Dockerfile
+++ Docker/nginx.Dockerfile
@@ -5,3 +5,9 @@ COPY ./nginx.conf /etc/nginx/nginx.conf
 
 # Copy build files from the React app
 COPY ./build /usr/share/nginx/html
+
+#expose port nginx
+EXPOSE 80
+
+# Perintah untuk menjalankan Nginx
+CMD ["nginx", "-g", "daemon off;"]
--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express');
-const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
 const app = express();
+const port = 3000; // Port backend, bisa disesuaikan
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Konfigurasi koneksi ke MySQL menggunakan variabel lingkungan
+const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"sepatu",
+  database:"cuci_sepatu"
 });
 
-app.listen(3000);
+
+db.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + db.threadId);
+});
+
+// Contoh endpoint untuk mendapatkan data dari MySQL
+app.get('/api/data', (req, res) => {
+  const query = 'SELECT * FROM cuci_sepatu'; // Ganti 'table_name' dengan nama tabel yang sebenarnya
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
